0216ec966e01a6772ffd384975ad05775e525d25
Bodi Stanciu <bogdanovidiu.stanciu@gmail.com>
2012-12-20 17:40:13 +0100

    changed fopen to curl, for compatibility with some servers (fopen blocked by default)

--- catalog/admin/includes/functions/localization.php
+++ catalog/admin/includes/functions/localization.php
@@ -11,12 +11,19 @@
 */
 
   function quote_oanda_currency($code, $base = DEFAULT_CURRENCY) {
-    $page = file('http://www.oanda.com/convert/fxdaily?value=1&redirected=1&exch=' . $code .  '&format=CSV&dest=Get+Table&sel_list=' . $base);
+
+  	$ch=curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'http://www.oanda.com/convert/fxdaily?value=1&redirected=1&exch=' . $code . '&format=CSV&dest=Get+Table&sel_list=' . $base);
+	curl_setopt($ch, CURLOPT_HEADER,0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_TRANSFERTEXT, 1);
+	$page= curl_exec($ch);
+	curl_close($ch);
 
     $match = array();
 
-    preg_match('/(.+),(\w{3}),([0-9.]+),([0-9.]+)/i', implode('', $page), $match);
-
+    preg_match('/(.+),(\w{3}),([0-9.]+),([0-9.]+)/i', $page, $match);
+    
     if (sizeof($match) > 0) {
       return $match[3];
     } else {
@@ -25,11 +32,18 @@
   }
 
   function quote_xe_currency($to, $from = DEFAULT_CURRENCY) {
-    $page = file('http://www.xe.net/ucc/convert.cgi?Amount=1&From=' . $from . '&To=' . $to);
+
+  	$ch=curl_init();
+  	curl_setopt($ch, CURLOPT_URL, 'http://www.xe.com/ucc/convert.cgi?Amount=1&From=' . $from . '&To=' . $to);
+  	curl_setopt($ch, CURLOPT_HEADER,0);
+  	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  	curl_setopt($ch, CURLOPT_TRANSFERTEXT, 1);
+  	$page= curl_exec($ch);
+  	curl_close($ch);
 
     $match = array();
-
-    preg_match('/[0-9.]+\s*' . $from . '\s*=\s*([0-9.]+)\s*' . $to . '/', implode('', $page), $match);
+    
+    preg_match('/[0-9.]+\s*' . $from . '\s*=\s*([0-9.]+)\s*' . $to . '/', $page, $match);
 
     if (sizeof($match) > 0) {
       return $match[1];
